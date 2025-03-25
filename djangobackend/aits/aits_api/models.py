885cec47847bf8d968e1fa6f7abf34ff333634df
sahyagit <hasahyasharif@gmail.com>
2025-03-25 22:53:15 +0300

    customer usermanager

--- djangobackend/aits/aits_api/models.py
+++ djangobackend/aits/aits_api/models.py
@@ -1,5 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
 
+
+# Custom User Manager
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
 # Create your models here.
 class roles(models.Model):
     ROLES = {
