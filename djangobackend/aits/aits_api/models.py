673c67e165817c7533bd27404ff788decd5fc353
Rodlin-Twebaze <158506886+call-me-rodney@users.noreply.github.com>
2025-03-25 23:29:04 +0300

    Applying audit trails

--- djangobackend/aits/aits_api/models.py
+++ djangobackend/aits/aits_api/models.py
@@ -1,6 +1,6 @@
 from django.db import models
+from simple_history.models import HistoricalRecords
 
-# Create your models here.
 class roles(models.Model):
     ROLES = {
         "S": "Student",
@@ -9,26 +9,28 @@ class roles(models.Model):
         "R": "Registrar",
     }
 
-    role = models.CharField(max_length=1,choices=ROLES)
+    role = models.CharField(max_length=1, choices=ROLES)
     roleID = models.IntegerField(primary_key=True)
     description = models.CharField(max_length=100)
+    history = HistoricalRecords()
 
     def __str__(self):
         return self.role
 
 class departments(models.Model):
     COLLEGE = {
-        'COCIS':'College of Computing and Information Sciences',
-        'CEDAT':'College of Engineering, Design, Art and Technology',
+        'COCIS': 'College of Computing and Information Sciences',
+        'CEDAT': 'College of Engineering, Design, Art and Technology',
     }
     deptID = models.IntegerField(primary_key=True)
     deptName = models.CharField(max_length=100)
-    college = models.CharField(max_length=10,choices=COLLEGE)
+    college = models.CharField(max_length=10, choices=COLLEGE)
     description = models.CharField(max_length=100)
+    history = HistoricalRecords()
 
     def __str__(self):
-        return self.deptName 
-    
+        return self.deptName
+
 class users(models.Model):
     userID = models.IntegerField(primary_key=True)
     roleID = models.ForeignKey(roles, on_delete=models.CASCADE)
@@ -36,17 +38,18 @@ class users(models.Model):
     firstName = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
+    history = HistoricalRecords()
 
     def __str__(self):
-        return self.name
-    
+        return self.firstName
+
 class issues(models.Model):
     STATUS = {
-        'CREATED':'CREATED',
-        'PENDING':'PENDING',
-        'OPEN':'OPEN',
-        'RESOLVED':'RESOLVED',
-        'CLOSED':'CLOSED',
+        'CREATED': 'CREATED',
+        'PENDING': 'PENDING',
+        'OPEN': 'OPEN',
+        'RESOLVED': 'RESOLVED',
+        'CLOSED': 'CLOSED',
     }
     CATEGORY = {
         'MM': 'Missing Marks',
@@ -58,27 +61,29 @@ class issues(models.Model):
     issueID = models.IntegerField(primary_key=True)
     userID = models.ForeignKey(users, on_delete=models.CASCADE)
     deptID = models.ForeignKey(departments, on_delete=models.CASCADE)
-    category = models.CharField(max_length=2,choices=CATEGORY)
+    category = models.CharField(max_length=2, choices=CATEGORY)
     description = models.CharField(max_length=255)
     status = models.CharField(max_length=20, choices=STATUS)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     assigned_to = models.CharField(max_length=100)
+    history = HistoricalRecords()
 
     def __str__(self):
-        return self.issue
-    
+        return self.description
+
 class notifications(models.Model):
     STATE = {
-        'READ':'READ',
-        'UNREAD':'UNREAD',
+        'READ': 'READ',
+        'UNREAD': 'UNREAD',
     }
     notID = models.IntegerField(primary_key=True)
     userID = models.ForeignKey(users, on_delete=models.CASCADE)
     issueID = models.ForeignKey(issues, on_delete=models.CASCADE)
     message = models.CharField(max_length=255)
-    state = models.CharField(max_length=10,choices=STATE)
+    state = models.CharField(max_length=10, choices=STATE)
     created_at = models.DateTimeField(auto_now_add=True)
+    history = HistoricalRecords()
 
     def __str__(self):
         return self.message
