5e78779a98573510fe641770cf5175188d47a2ec
sahyagit <hasahyasharif@gmail.com>
2025-03-26 11:37:48 +0300

    tokenrefresh urls

--- djangobackend/aits/aits_api/urls.py
+++ djangobackend/aits/aits_api/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     # Authentication endpoints
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
