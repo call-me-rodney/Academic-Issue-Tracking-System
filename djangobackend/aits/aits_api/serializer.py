46169021c79c6eb5542afd211f26c85c7d3eff16
sahyagit <hasahyasharif@gmail.com>
2025-03-26 08:50:50 +0300

    user serializer and validating password

--- djangobackend/aits/aits_api/serializer.py
+++ djangobackend/aits/aits_api/serializer.py
@@ -5,11 +5,24 @@ from .models import *
 
 User = get_user_model()
 
-class userSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = users
-        fields = ["userID","roleID","name","email","password"]
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
 
+    class Meta:
+        model = User
+        fields = ["userID","roleID","name","email","password"]
+        extra_kwargs = {
+            'first_name': {'required': True},
+            'last_name': {'required': True},
+            'unique_number': {'required': True}
+        }
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs.pop('password2'):
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        return attrs
+    
 class roleSerializer(serializers.ModelSerializer):
     class Meta:
         model = roles
