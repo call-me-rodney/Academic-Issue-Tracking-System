5c44e30044c2839d3e37a53fc81dcae9bf6856a7
Anguzu-Emmanuel <anguzuemmanuels@gmail.com>
2025-04-02 21:33:58 +0300

    Add files via upload
    Signup account

---
+++ Signup.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import './SignUp.css'
+import { NavLink } from 'react-router-dom'
+
+const SignUp = () => {
+  return (
+    <div className="container">
+        <form action="#" className="form">
+        <h1>Create an account</h1>
+        <div className="box">
+        <div className="input">
+        <label for="name">First name</label>
+        <input className="input-box" required type="text" placeholder="Enter your name" name="name"/>
+        </div>
+        <div className="input">
+        <label for="name">Last name</label>
+        <input className="input-box" required type="text" placeholder="Enter your name" name="name"/>
+        </div></div>
+        <div className="input">
+        <label for="name">Your email</label>
+        <input className="input-box" required type="email" placeholder="Enter your email" name="name"/>
+        </div>
+        <div className="input">
+        <label for="name">Your password</label>
+        <input className="input-box" required type="password" placeholder="Enter your name" name="name"/>
+        </div>
+        <div className="input">
+        <label for="name">Confirm password</label>
+        <input className="input-box" repeat type="password" placeholder="Enter your name" name="name"/>
+        </div>
+        <div className="input">
+        <label for="name">Select Role</label>
+        <select className="dropdown">
+          <option>student</option>
+          <option>Lecturer</option>
+          <option>Administrator</option>
+          <option>Academic Register</option>
+          </select>
+          </div>
+        <div className="input">
+        <input className="input-box-1" type="submit" value="Create account"/>
+        </div>
+        <div className="input">
+        Already have an account? <NavLink to='/login'>Login here</NavLink>
+        </div>
+        </form>
+      </div>
+  )
+}
+
+export default SignUp
