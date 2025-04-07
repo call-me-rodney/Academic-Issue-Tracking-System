24146b56dbc55448f1f03297c5fb05bd223c6da2
call-me-rodney <kwmrodney@gmail.com>
2025-04-07 21:39:14 +0300

    reverting frontend structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,81 +1,16 @@
-import { Routes, Route } from "react-router-dom"
-import Login from "./components/Login"
-import Signup from "./components/Signup"
-import Dashboard from "./components/Dashboard"
-import Students_profile from "./Students_profile"
-import Students_results from "./Students_results"
-import Students_account from "./Students_account"
-import Lecturers_acoount from "./Lecturers_acoount"
-import Course_details from "./Course_details"
-import IssueSubmit from "./IssueSubmit"
-import ProtectedRoute from "./components/ProtectedRoute"
-import "./App.css"
+import React from 'react';
+import './App.css';
+//import IssueSubmit from './IssueSubmit'; // Import the IssueSubmit component
+//import Login from './componets/Login';
+import Allroutes from './Routes/Allroutes';
 
-function App() {
+const App = () => {
   return (
-    <Routes>
-      {/* Public routes */}
-      <Route path="/" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-
-      {/* Protected routes */}
-      <Route
-        path="/dashboard"
-        element={
-          
-            <Dashboard />
-          
-        }
-      />
-      <Route
-        path="/profile"
-        element={
-       
-            <Students_profile />
-         
-        }
-      />
-      <Route
-        path="/results"
-        element={
-          
-            <Students_results />
-        
-        }
-      />
-      <Route
-        path="/student-account"
-        element={
-          
-            <Students_account />
-          
-        }
-      />
-      <Route
-        path="/lecturer-account"
-        element={
-         
-            <Lecturers_acoount />
-         
-        }
-      />
-      <Route
-        path="/course-details"
-        element={
-          
-            <Course_details />
-        }
-      />
-      <Route
-        path="/submit-issue"
-        element={
-          
-            <IssueSubmit />
-        }
-      />
-    </Routes>
+    <>
+   
+    <Allroutes/>
+    </>
   )
 }
 
-export default App
-
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,17 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
-import { AuthProvider } from "./context/AuthContext"
-import App from "./App"
-import "./index.css"
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import  './componets/Signup.css'
+import './componets/Login.css'
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <StrictMode>
+    <App />
+  </StrictMode>
+  </BrowserRouter>
+
+
 )
-
