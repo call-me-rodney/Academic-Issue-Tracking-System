7d701dbcd2cd383ca1a03922a7874cd5f5dfb4f3
Beatrice-2000 <bakello475@gmail.com>
2025-04-09 14:17:18 +0300

    Update Course_details.jsx

--- client/src/componets/Course_details.jsx
+++ client/src/componets/Course_details.jsx
@@ -1,33 +1,40 @@
-import React from 'react'
-import './course_details.css'
+import React from 'react';
+import './course_details.css';
 
 const Course_details = () => {
   return (
     <div>
-         <div class="container">
+         <div className="container">
         <h2>Year 1 - Semester I</h2>
         <table>
+          <thead>
             <tr>
                 <th>CODE</th>
                 <th>COURSE TITLE</th>
                 <th>CATEGORY</th>
                 <th>CREDIT UNITS</th>
             </tr>
+          </thead>
+          <tbody>
             <tr><td>IST1204</td><td>Systems Analysis and Design</td><td>Core</td><td>4</td></tr>
             <tr><td>CSC1206</td><td>Operating Systems</td><td>Core</td><td>4</td></tr>
             <tr><td>CSC1201</td><td>Probability & Statistics</td><td>Core</td><td>4</td></tr>
             <tr><td>CSC1206</td><td>Software Development Project</td><td>Core</td><td>4</td></tr>
             <tr><td>CSC1207</td><td>Data Structure and Algorithms</td><td>Core</td><td>4</td></tr>
-        </table>
+          </tbody>
+         </table>
 
         <h2>Year 2 - Semester II</h2>
         <table>
+          <thead>
             <tr>
                 <th>CODE</th>
                 <th>COURSE TITLE</th>
                 <th>CATEGORY</th>
                 <th>CREDIT UNITS</th>
             </tr>
+          </thead>
+          <tbody>
             <tr><td>CSC2206</td><td>Automata, Complexity & Computability</td><td>Core</td><td>3</td></tr>
             <tr><td>IST2203</td><td>Research Methodology</td><td>Core</td><td>4</td></tr>
             <tr><td>CSC2207</td><td>Introduction to Machine Learning</td><td>Core</td><td>4</td></tr>
@@ -35,16 +42,20 @@ const Course_details = () => {
             <tr><td>CSC2207</td><td>Robotics</td><td>Elective</td><td>4</td></tr>
             <tr><td>CSC2208</td><td>Software Quality and Verification</td><td>Elective</td><td>4</td></tr>
             <tr><td>CSC2218</td><td>Software Construction</td><td>Elective</td><td>4</td></tr>
-        </table>
+       </tbody>
+     </table>
 
         <h2>Year 3 - Semester II</h2>
         <table>
+          <thead>
             <tr>
                 <th>CODE</th>
                 <th>COURSE TITLE</th>
                 <th>CATEGORY</th>
                 <th>CREDIT UNITS</th>
             </tr>
+            </thead>
+          <tbody>
             <tr><td>CSC2305</td><td>Compiler Design</td><td>Core</td><td>3</td></tr>
             <tr><td>CSC2306</td><td>Computer Security</td><td>Core</td><td>3</td></tr>
             <tr><td>CSC2317</td><td>Emerging Trends in Computer Science</td><td>Core</td><td>3</td></tr>
@@ -52,10 +63,11 @@ const Course_details = () => {
             <tr><td>BSE2206</td><td>Data Communications</td><td>Elective</td><td>4</td></tr>
             <tr><td>BSE2208</td><td>Data Warehousing and Business Intelligence</td><td>Elective</td><td>4</td></tr>
             <tr><td>BSE2202</td><td>Distributed Systems Development</td><td>Elective</td><td>4</td></tr>
-        </table>
+         </tbody>
+      </table>
     </div>
     </div>
   )
 }
 
-export default Course_details
+export default Course_details;
