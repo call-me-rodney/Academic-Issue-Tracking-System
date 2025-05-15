a4f9fa65bb57b1057f70031e993d480ae5c635af
Rodlin-Twebaze <twebazerodlin9@gmail.com>
2025-05-15 10:47:33 +0300

    changed debug setting

--- djangobackend/aits/aits/settings.py
+++ djangobackend/aits/aits/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9il=3+e&r7l-6$o^6q_3$7z(sa^gm$1u8*#1=w+riw*#2hp#-j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
