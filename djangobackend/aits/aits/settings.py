80a2046ffb714e2a5735da1295f1c1882de7c55f
Rodlin-Twebaze <twebazerodlin9@gmail.com>
2025-05-15 11:11:35 +0300

    Added STORAGES

--- djangobackend/aits/aits/settings.py
+++ djangobackend/aits/aits/settings.py
@@ -148,6 +148,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT=BASE_DIR /'staticfiles'
+
+STORAGES={
+    'staticfiles':{
+        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    }
+}
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
